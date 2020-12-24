0e81cb64ac58482b932edae40e64fc7a68a3b77c
andrei <bl@n.k>
2020-12-24 12:47:52 +0200

    Added namespace

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 
+app_name = 'chat'
 urlpatterns = [
     path('operator/<str:pk>/', operator, name='operator'),
     path('client/<str:pk>/', client, name='client'),
