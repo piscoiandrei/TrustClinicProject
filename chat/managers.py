b762c93c11632aa41b4cc42d92e03aeb7ad16eaf
piscoiandrei <piscoiandrei27@gmail.com>
2020-12-24 22:06:32 +0200

    fixed syntax error

--- chat/managers.py
+++ chat/managers.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 User = get_user_model()
 
@@ -9,13 +10,13 @@ class MessageManager(models.Manager):
         return self.filter(source=user1, endpoint=user2)
 
     def from_to_id(self, id1, id2):
-        user1 = User.get_object_or_404(pk=id1)
-        user2 = User.get_object_or_404(pk=id2)
+        user1 = get_object_or_404(User, pk=id1)
+        user2 = get_object_or_404(User, pk=id2)
         return self.filter(source=user1, endpoint=user2)
 
     def from_to_email(self, email1, email2):
-        user1 = User.get_object_or_404(email=email1)
-        user2 = User.get_object_or_404(email=email2)
+        user1 = get_object_or_404(User, pk=id1)
+        user2 = get_object_or_404(User, pk=id2)
         return self.filter(source=user1, endpoint=user2)
 
     def create_from_to_obj(self, user1, user2, content):
@@ -23,7 +24,7 @@ class MessageManager(models.Manager):
         msg.save(using=self._db)
 
     def create_from_to_id(self, id1, id2, content):
-        user1 = User.get_object_or_404(pk=id1)
-        user2 = User.get_object_or_404(pk=id2)
+        user1 = get_object_or_404(User, pk=id1)
+        user2 = get_object_or_404(User, pk=id2)
         msg = self.model(source=user1, endpoint=user2, content=content)
         msg.save(using=self._db)
