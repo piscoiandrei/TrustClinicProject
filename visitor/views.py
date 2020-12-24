2cf3d861f37dd926ead3a6bdbfbf86316ab07055
andrei <bl@n.k>
2020-12-24 09:54:47 +0200

    login redirect

--- visitor/views.py
+++ visitor/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.generic.base import TemplateResponse
 
 
@@ -7,6 +8,6 @@ def home(request):
     return TemplateResponse(request, 'visitor/home.html', {})
 
 
-# this should redirected to the unexisting login page, handled by another app
 def login_redirect(request):
-    return redirect('/')
+    # add extra request request if needed
+    return redirect(reverse_lazy('accounts:login'))
