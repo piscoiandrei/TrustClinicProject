d531f2eef7a51eb4c9d54adc6bad7b3d879209a8
piscoiandrei <piscoiandrei27@gmail.com>
2020-12-25 08:58:13 +0200

    removed an import

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin, AdminPasswordChangeForm
-from django.contrib.auth.models import Group
 
 from .forms import UserAdminChangeForm, UserAdminCreationForm
 
