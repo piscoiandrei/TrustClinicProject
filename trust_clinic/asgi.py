0f11810da646f5563bd8134e39ef4ea182ff2495
piscoiandrei <piscoiandrei27@gmail.com>
2021-01-18 21:10:36 +0200

    .

--- trust_clinic/asgi.py
+++ trust_clinic/asgi.py
@@ -13,7 +13,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trust_clinic.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trust_clinic.settings')
 
 application = ProtocolTypeRouter(
     {
