623e545154a14a7f586f3b7b13e3100485e4e59e
piscoiandrei <piscoiandrei27@gmail.com>
2021-01-18 23:01:30 +0200

    fixed visitor redirect

--- trust_clinic/middleware.py
+++ trust_clinic/middleware.py
@@ -55,5 +55,7 @@ class LoginRequired(MiddlewareMixin):
                     request.user.is_superuser == False)):
                 return self.redirect_role(request.user)
         else:
+            if path == '/':
+                return redirect('visitor:home')
             if not self.contains_ignored(path):
                 return redirect('accounts:login')
