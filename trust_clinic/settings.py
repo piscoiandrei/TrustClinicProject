7f863121ec402eb14483d928ee0291d0076bee81
piscoiandrei <piscoiandrei27@gmail.com>
2021-01-18 22:44:32 +0200

    .

--- trust_clinic/settings.py
+++ trust_clinic/settings.py
@@ -1,11 +1,12 @@
 import os
 from pathlib import Path
+import django_heroku
 
 # Note: default is parent.parent to go to the main dir now
 # we need to go one level higher
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = '#all!i!w&$2#$$)ghtpra8wbl#vu@&+5b$crv28t&3fesom8)1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 ALLOWED_HOSTS = ['trustclinic.herokuapp.com']
@@ -168,5 +169,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'django.email.service@gmail.com'
-EMAIL_HOST_PASSWORD = 'ed6d37c1eab39ada'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+django_heroku.settings(locals())
