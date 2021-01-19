a8151435a3b3f08a6f6fb7ea8d5f87912633e82a
piscoiandrei <piscoiandrei27@gmail.com>
2021-01-19 13:46:27 +0200

    .

--- trust_clinic/settings.py
+++ trust_clinic/settings.py
@@ -185,5 +185,6 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 django_heroku.settings(locals())
