3104afdae6bdf179db64e8ea5570fa402b678b43
piscoiandrei <piscoiandrei27@gmail.com>
2021-01-19 12:30:52 +0200

    .

--- trust_clinic/settings.py
+++ trust_clinic/settings.py
@@ -14,7 +14,6 @@ ALLOWED_HOSTS = ['trustclinic.herokuapp.com']
 INSTALLED_APPS = [
     # installed
     'channels',
-    'whitenoise.runserver_nostatic',
 
     # my apps
     'accounts',
@@ -37,12 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # installed
-    'django_cleanup.apps.CleanupConfig'
+    'storages',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -162,7 +160,6 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # SMTP CONFIG
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -171,5 +168,12 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
