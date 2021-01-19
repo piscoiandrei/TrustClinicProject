6759fe03290de685bc57f1f8d8af84781657ec2f
piscoiandrei <piscoiandrei27@gmail.com>
2021-01-19 09:46:53 +0200

    .

--- trust_clinic/asgi.py
+++ trust_clinic/asgi.py
@@ -8,16 +8,18 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trust_clinic.settings')
+app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trust_clinic.settings')
-
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": app,
         "websocket": AuthMiddlewareStack(
             URLRouter(
                 chat.routing.websocket_urlpatterns
