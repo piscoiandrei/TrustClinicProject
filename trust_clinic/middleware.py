3a1277b0f7266bbb93ac68487c0f176ab83df820
piscoiandrei <piscoiandrei27@gmail.com>
2021-01-19 14:58:13 +0200

    reconfigured LoginRequired

--- trust_clinic/middleware.py
+++ trust_clinic/middleware.py
@@ -39,34 +39,3 @@ class LoginRequired(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwargs):
         path = request.path
         user = request.user
-
-        if not user.is_authenticated:
-            # if user tries to access a forbbiden url it gets redirected
-            if not self.match_path(path, settings.LOGGED_OUT_ONLY_URLS):
-                return redirect('accounts:login')
-
-        if user.is_authenticated:
-            # if the url requires special permissions
-            if not self.match_path(path, settings.LOGIN_REQUIRED_URLS):
-
-                if user.is_client:
-                    # checking to see if the client has permission for the url
-                    if not self.match_path(path, settings.CLIENT_URLS):
-                        return redirect('client:dashboard')
-
-                if user.is_operator:
-                    if not self.match_path(path, settings.OPERATOR_URLS):
-                        # starting a new operator chat session
-                        return redirect('chat:operator_session')
-
-                if user.is_doctor:
-                    if not self.match_path(path, settings.DOCTOR_URLS):
-                        return redirect('doctor:dashboard')
-
-                if user.is_staff or user.is_superuser:
-                    # staff and superuser users have permissions in all the
-                    # urls that the other users don't have
-
-                    # if the function found a match we redirect
-                    if self.match_all(path):
-                        return redirect(resolve_url('/admin/'))
