054c1b22c54dd73c1b6c6543dbc3df3db566095f
piscoiandrei <piscoiandrei27@gmail.com>
2021-01-19 08:45:33 +0200

    .

--- trust_clinic/settings.py
+++ trust_clinic/settings.py
@@ -111,7 +111,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [(os.environ.get('REDIS_TLS_URL'), 6379)],
         },
     },
 }
